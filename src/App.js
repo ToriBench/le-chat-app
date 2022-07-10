3e120149992d4c79d9d57e06bd0499728b7d3aa6
ToriBench <victoriab@bbd.co.za>
2022-07-10 15:42:29 +0200

    Auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,195 @@
-import React from 'react';
+import './App.css';
+import { Amplify } from 'aws-amplify';
 
-import ChatShell from './containers/shell/ChatShell';
+import {
+  Authenticator,
+  View,
+  Text,
+  Heading,
+  Image,
+  useAuthenticator,
+  Button,
+  useTheme,
+} from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-const App = () => {
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+const components = {
+  Header() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Image
+          alt="Le Chat Logo"
+          src="https://lh3.googleusercontent.com/veuz4T5QRZ3qLqro5K_KnG7ztuNHKtF72lQ7xCttIpblNIDGQC7VJVgo-bQih2epOstwbt4fREB1OVeFl_u4kQdIHypdyjInecz6_H11f5fGaH751qYEKCr4xF20AANgrw=w1280"
+          maxHeight={250}
+        />
+      </View>
+    );
+  },
+
+  Footer() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Text color={tokens.colors.neutral[80]}>
+          &copy; All Rights Reserved
+        </Text>
+      </View>
+    );
+  },
+
+  SignIn: {
+    Header() {
+      const { tokens } = useTheme();
+
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Sign in to your account
+        </Heading>
+      );
+    },
+    Footer() {
+      const { toResetPassword } = useAuthenticator();
+
+      return (
+        <View textAlign="center">
+          <Button
+            fontWeight="normal"
+            onClick={toResetPassword}
+            size="small"
+            variation="link"
+          >
+            Reset Password
+          </Button>
+        </View>
+      );
+    },
+  },
+
+  SignUp: {
+    Header() {
+      const { tokens } = useTheme();
+
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Create a new account
+        </Heading>
+      );
+    },
+    Footer() {
+      const { toSignIn } = useAuthenticator();
+
+      return (
+        <View textAlign="center">
+          <Button
+            fontWeight="normal"
+            onClick={toSignIn}
+            size="small"
+            variation="link"
+          >
+            Back to Sign In
+          </Button>
+        </View>
+      );
+    },
+  },
+  ConfirmSignUp: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+  SetupTOTP: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+  ConfirmSignIn: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+  ResetPassword: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+  ConfirmResetPassword: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        >
+          Enter Information:
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text>Footer Information</Text>;
+    },
+  },
+};
+
+export default function App() {
   return (
-    <ChatShell />
+    <Authenticator components={components}>
+      {({ signOut }) => <button onClick={signOut}>Sign out</button>}
+    </Authenticator>
   );
 }
-
-export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,21 +1,8 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-
-import './index.css';
-import rootReducer from './store/reducers';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-const store = createStore(rootReducer);
-
-test('App loads with default state', () => {
-  const component = renderer.create(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
